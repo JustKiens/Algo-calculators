7606b2d9c138ee7cdd05f87d2f944e2cd6e572de
JustKiens <145907186+JustKiens@users.noreply.github.com>
2024-11-04 23:24:37 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import FutureValueOrdinaryAnnuity from './pages/future/futureValueOrdinaryAnnuity';
-import FutureValueAnnuityDue from './pages/future/futureValueAnnuityDue';
+import FutureValueAnnuityDue from './pages/future/futureValueAnnuityDue'; // This import can be used later if needed
 import LandingPage from './pages/landingpage'; // Import the new LandingPage component
 import './index.css'; // Ensure Tailwind CSS is imported
 
@@ -10,11 +10,16 @@ function App() {
       {/* Navbar */}
       <nav className="bg-gray-800 p-4">
         <div className="container mx-auto flex justify-between items-center">
-          <Link to="/future-value-ordinary-annuit" className="text-white text-2xl font-bold">
-          Future Value of Simple Annuity
+          <Link to="/" className="text-white text-2xl font-bold">
+            Future Value of Ordinary Annuity
           </Link>
-          <div>
-          </div>
+          <Link to="/future-value-ordinary-annuity" className="text-white text-2xl font-bold">
+            Future Value of Ordinary Annuity
+          </Link>
+          {/* Uncomment below if you need a link for Future Value Annuity Due */}
+          {/* <Link to="/future-value-annuity-due" className="text-white text-2xl font-bold">
+            Future Value of Annuity Due
+          </Link> */}
         </div>
       </nav>
 
@@ -23,6 +28,8 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} /> {/* Use the LandingPage component */}
           <Route path="/future-value-ordinary-annuity" element={<FutureValueOrdinaryAnnuity />} />
+          {/* Uncomment below if you want to include FutureValueAnnuityDue */}
+          {/* <Route path="/future-value-annuity-due" element={<FutureValueAnnuityDue />} /> */}
         </Routes>
       </div>
     </Router>
